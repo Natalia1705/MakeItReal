cab94ff137666b69bcc8a638ddc95aa5d131f302
Natalia <lau.nat.her.her@gmail.com>
2021-11-16 14:04:52 -0600

    actualizaciÃon ejercicio1

--- 05.Closures/index.js
+++ 05.Closures/index.js
@@ -1,11 +1,11 @@
-Ejercicio 1
+// Ejercicio 1
 
-Estás diseñando un sistema de facturación. Debes representar la lista de facturas,
-donde cada factura está compuesta de un número, el nombre del cliente, la fecha, y unos ítems.
-Cada ítem tiene un id de producto, un precio por unidad y una cantidad.
+// Estás diseñando un sistema de facturación. Debes representar la lista de facturas,
+// donde cada factura está compuesta de un número, el nombre del cliente, la fecha, y unos ítems.
+// Cada ítem tiene un id de producto, un precio por unidad y una cantidad.
 
-¿Cómo representarías esta lista utilizando arreglos y objetos?
-Lista de facturas
+// ¿Cómo representarías esta lista utilizando arreglos y objetos?
+// Lista de facturas
 var arrayInvoice = []
 
 //funcion que permite agregar facturas a la lista de facturas
@@ -20,7 +20,7 @@ function addInvoice( number, customer, date, items ){
     addItems();
     //funcion que permite agregar items a la factura
     function addItems (){
-        invoice.items = items.map( ({ id, price, quantity }) => { return { id, price ,quantity } })
+        invoice.items = items.map( ({ id, price, quantity }) => ( { id, price ,quantity  }))
     }
     //Agregamos facturas al arreglo de facturas
     arrayInvoice.push(invoice)
@@ -28,16 +28,16 @@ function addInvoice( number, customer, date, items ){
 
 addInvoice(1, "name", '27/10/2021', [{ id: 1, price: 25, quantity: 30 },{ id: 2, price: 20, quantity: 30 } ] )
 
+console.log(arrayInvoice)
+// Ejercicio 2
 
-Ejercicio 2
+// Escribir una función crearContador que retorne una nueva función que incremente un número y
+// lo retorne cada vez que es invocada:
 
-Escribir una función crearContador que retorne una nueva función que incremente un número y
-lo retorne cada vez que es invocada:
-
-const contar = createContador()
-contar() // 1
-contar() // 2
-contar() // 3
+// const contar = createContador()
+// contar() // 1
+// contar() // 2
+// contar() // 3
 
 function createCount () {
     let count = 0;
@@ -55,22 +55,22 @@ console.log(counter1()); // 4
 
 
 
-Recuerda de hacer uso de closures para evitar el uso de variables globales.
-Ejercicio 3
+// Recuerda de hacer uso de closures para evitar el uso de variables globales.
+// Ejercicio 3
 
-Escribir una función createGame que retorne una nueva función que reciba un número y permita 
-adivinar un número secreto del 1 al 100. Si el número es mayor al número secreto la función retorna 
-la cadena “Muy alto!”, si el número es menor retorna la cadena “Muy bajo!”. Si el número es el correcto 
-retorna “Lo adivinaste, felicitaciones!”
+// Escribir una función createGame que retorne una nueva función que reciba un número y permita 
+// adivinar un número secreto del 1 al 100. Si el número es mayor al número secreto la función retorna 
+// la cadena “Muy alto!”, si el número es menor retorna la cadena “Muy bajo!”. Si el número es el correcto 
+// retorna “Lo adivinaste, felicitaciones!”
 
-const guess = createGame() // numero secreto: 5
-guess(8) // "Muy alto!"
-guess(4) // "Muy bajo!"
-guess(5) // "Lo adivinaste, felicitaciones!"
+// const guess = createGame() // numero secreto: 5
+// guess(8) // "Muy alto!"
+// guess(4) // "Muy bajo!"
+// guess(5) // "Lo adivinaste, felicitaciones!"
 
-Bonus: crear una aplicación de consola para poder jugar el juego
+// Bonus: crear una aplicación de consola para poder jugar el juego
 
-const readline = require("readline");
+// const readline = require("readline");
 
 function createGame(){
     let secret = Math.floor(Math.random() * (100 - 1) + 1);
