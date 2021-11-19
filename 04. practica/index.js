37497a83be893bd7da41d5a03c861495ad24b5e9
Natalia Hernandez <85592301+Natalia1705@users.noreply.github.com>
2021-11-19 07:28:58 -0600

    Revert "actualización"

--- 04. practica/index.js
+++ 04. practica/index.js
@@ -1,16 +1,16 @@
 // Ejercicio 1
 
-const recipe = {
-  name: "",
-  ingredients: [
-    { name: "Pan", quantity: 2 },
-    { name: "Jamón", quantity: 1 },
-    { name: "Queso", quantity: 1 },
-  ]
-}
+// const recipe = {
+//   name: "",
+//   ingredients: [
+//     { name: "Pan", quantity: 2 },
+//     { name: "Jamón", quantity: 1 },
+//     { name: "Queso", quantity: 1 },
+//   ]
+// }
 //     Imprimir la cantidad del primer ingrediente.
 
-console.log(recipe.ingredients[0].quantity)
+// console.log(recipe.ingredients[0].quantity)
 
 //     Imprimir los ingredientes con el siguiente formato:
 
@@ -18,9 +18,9 @@ console.log(recipe.ingredients[0].quantity)
 // 1 de Jamón
 // 1 de Queso
 
-     let receta = Object.keys(recipe.ingredients).map(e => {
-      console.log(`${recipe.ingredients[e].quantity} de ${recipe.ingredients[e].name}`)
-        })
+    //  let receta = Object.keys(recipe.ingredients).map(element => {
+    //   console.log (` ${recipe.ingredients[element].quantity} de ${recipe.ingredients[element].name}` )
+    //     });
 
 
 // Ejercicio 2
@@ -28,7 +28,6 @@ console.log(recipe.ingredients[0].quantity)
 // Escribir una función capitalizar que reciba un string y retorne otro string que capitalice cada palabra de la frase.
 
 // capitalizar("hola mundo") // "Hola Mundo"
-
 
 
 // function capitalize(str){
@@ -45,12 +44,13 @@ console.log(recipe.ingredients[0].quantity)
 // https://leetcode.com/problems/length-of-last-word/
 
 
-function lastItem(str) {
-     let arr = str.trim().split(" ")
-     let arr2 = arr[arr.length-1];
-     return arr2.length;
-        }
-   console.log(lastItem( " Hello  Worldaa"));
+// function lastItem(string) {
+//      let arr = string.trim().split(" ")
+//      let arr2 = arr[arr.length-1];
+//      return arr2.length;
+//         }
+
+//    console.log(lastItem( " Hello  Worldaa"));
 
 
 // Ejercicio 4
@@ -65,7 +65,7 @@ function Capitalize(str) {
                 
               }
 
-console.log(Capitalize("hola"))
+console.log(Capitalize("anita lava la tina"))
 
 
 
@@ -80,31 +80,29 @@ console.log(Capitalize("hola"))
 // Ejercicio 5 - Colores hexadecimales
 
 // Escribe una expresión regular que verifique si una cadena es un color hexadecimal válido como #f00 o #bada55. Recuerda que un color hexadecimal puede tener 3 o 6 caracteres, y cada caracter puede ir de 0 a 9 y de A a F.
-const regex = /[0-9A-F]/ig
-console.log(regex.test("#f00s"));
+
+// /[0-9a-fA-F]+/
+
 // Ejercicio 6 - Fechas
 
 // Escribe una expresión regular que verifique si una cadena es una fecha válida, por ejemplo, 12/12/2012 o 24/08/1982, etc.
 
 // Intenta primero crear una expresión que valide el formato únicamente, es decir, que comience con dos dígitos, seguido de un /, dos dígitos, otro / y cuatro dígitos, sin importar si son fechas inválidas como 45/98/9234.
-const regex1 = /(\d{2})\/(\d{2})\/(\d{4})/
-console.log(regex1.test("45/98/9234"));
 
 // Empieza a agregarle más restricciones, por ejemplo, el primer dígito puede ser un 0, 1, 2 o 3 únicamente. Eso podría generar días inválidos como 36 pero es un avance.
 
-const regex2 = /([0][1-9]|3[01])\/([0][1-9]|1[0-2])\/([0-9]{4})/
-console.log(regex2.test("31/12/9234"));
-
 // Sigue agregando restricciones para ver hasta qué punto llegas. Sin embargo, ten en cuenta que no vas a encontrar una expresión regular perfecta porque hay meses con diferentes días.
+
+// ^\d{4}([\-/.])(0?[1-9]|1[1-2])\1(3[01]|[12][0-9]|0?[1-9])$
+
 // Ejercicio 7 - Email
 
 // Escribe una expresión regular que verifique si una dirección de email es válida.
+
 // Puedes empezar por una expresión que verifique que comience con al menos un caracter, que tenga una @ y que tenga más caracteres, seguido de un punto, seguido de más caracteres.
 
-const regex3 = /.+@.+\..+/
-console.log(regex3.test("hola@3k.jhg"));
 // Así como en el ejercicio anterior no vas a encontrar una expresión regular perfecta pero te puede ayudar a filtrar emails que claramente sean inválidos.
 
-// /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/
 
+// /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/
 
